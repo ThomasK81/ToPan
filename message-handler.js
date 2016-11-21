71ff253ab6750a2518dbe8c8570a6860b3fe604a
J. R. Schmid <jrs+git@weitnahbei.de>
2016-11-21 17:11:54 +0100

    These are not used anywhere; moving to subfolder of their own.

--- message-handler.js
+++
@@ -1,9 +0,0 @@
-// This recieves messages of type "testmessage" from the server.
-// See http://shiny.rstudio.com/gallery/server-to-client-custom-messages.html
-// for details
-
-Shiny.addCustomMessageHandler("testmessage",
-  function(message) {
-    alert(JSON.stringify(message));
-  }
-);
